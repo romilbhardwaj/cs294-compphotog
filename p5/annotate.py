cb5b16ef97276a4fd4be4e42e6840c1de24e26c1
Romil <romil.bhardwaj@gmail.com>
2020-11-19 15:47:45 -0800

    Init P5 attempt

---
+++ p5/annotate.py
@@ -0,0 +1,27 @@
+# Use this script to annotate keypoints on an image
+import argparse
+import csv
+import os
+import matplotlib.pyplot as plt
+import skimage.io as skio
+
+parser = argparse.ArgumentParser(description='Keypoint annotation script.')
+parser.add_argument('imgpath', type=str, help='Path to the img')
+parser.add_argument('-n', type=int, default=10, help='Number of points to annotate')
+
+args = parser.parse_args()
+n_points = args.n
+img_path = args.imgpath
+
+img_filename = os.path.splitext(os.path.basename(img_path))[0]
+
+image = skio.imread(img_path)
+plt.imshow(image)
+points = plt.ginput(n_points, timeout=0, show_clicks=True)
+plt.close()
+
+csv_path = os.path.join(os.path.dirname(img_path), f'{img_filename}.csv')
+
+with open(csv_path, "w", newline='') as f:
+    writer = csv.writer(f)
+    writer.writerows(points)
