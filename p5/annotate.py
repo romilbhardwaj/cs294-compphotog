82b8ebd2db185900a4bd9bce1c2853ea35660c04
Romil <romil.bhardwaj@gmail.com>
2020-11-19 21:19:50 -0800

    Almost done

--- p5/annotate.py
+++ p5/annotate.py
@@ -8,10 +8,12 @@ import skimage.io as skio
 parser = argparse.ArgumentParser(description='Keypoint annotation script.')
 parser.add_argument('imgpath', type=str, help='Path to the img')
 parser.add_argument('-n', type=int, default=10, help='Number of points to annotate')
+parser.add_argument('-s', type=str, default="", help='output file suffix')
 
 args = parser.parse_args()
 n_points = args.n
 img_path = args.imgpath
+out_suffix = args.s
 
 img_filename = os.path.splitext(os.path.basename(img_path))[0]
 
@@ -20,7 +22,7 @@ plt.imshow(image)
 points = plt.ginput(n_points, timeout=0, show_clicks=True)
 plt.close()
 
-csv_path = os.path.join(os.path.dirname(img_path), f'{img_filename}.csv')
+csv_path = os.path.join(os.path.dirname(img_path), f'{img_filename}{out_suffix}.csv')
 
 with open(csv_path, "w", newline='') as f:
     writer = csv.writer(f)
